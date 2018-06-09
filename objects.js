375b3b8f39298934ce1ffba1a469a2a8818f161d
jason.radin@gmail.com <jason.radin@gmail.com>
2018-06-09 17:52:47 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -8,3 +8,9 @@ function removeFromPlaylist(playlist, artistName) {
   delete playlist.artistName; 
   return playlist; 
 }
+
+var playlist = { artistName: "songTitle" }; <<< also dont forget the spaces here 
+
+function updatePlaylist(playlist, artistName, songTitle) {
+   return Object.assign( playlist, { [artistName]: songTitle })
+}
